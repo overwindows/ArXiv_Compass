e847b0e97b4d833df1e4d36c3a0f6fae1c448d27
overwindows <overwindows@gmail.com>
2016-03-14 18:13:35 +0800

    sites_0

--- App/sites.py
+++ App/sites.py
@@ -16,6 +16,11 @@ from env import render
 class sites:
     def GET(self):
         #offices = model.get_offices_ex()
+        
+        districts = list(model.get_districts())
+        zones = list(model.get_zones())
+        plazas = list(model.get_plazas())
+        offices = list(model.get_offices_ex())
            
         return render.sites(districts, zones, plazas, offices)
         #return render.index(offices,uid,msgs,islogin,web.ctx.session.nickname,web.ctx.session.headimgurl)
